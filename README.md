7570b65fca547b5e00fc44ad80658815a594531b
hulkfolk <lvdensun@gmail.com>
2020-04-18 23:54:12 +0800

    update data cleaning script

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-sudo apt-get install python3.6
-sudo apt-get install python3-pip
-sudo python3.6 -m pip install flask
-sudo python3.6 -m pip install flask_api
-sudo python3.6 -m pip install mysql-connector-python
+sudo apt-get install python3.6  
+sudo apt-get install python3-pip  
+sudo python3.6 -m pip install flask  
+sudo python3.6 -m pip install flask_api  
+sudo python3.6 -m pip install mysql-connector-python  
 
 
-upload folders to ec2:
-scp -i E0383718Key.pem -r app_server ubuntu@[public-ip]:~/app_server
+upload folders to ec2:  
+scp -i E0383718Key.pem -r app_server ubuntu@[public-ip]:~/app_server  
 
-
-(kill the service running on port 80 if there is any --> sudo lsof -i:80)
-sduo python3 ~/app_server/run.py
+(kill the service running on port 80 if there is any --> sudo lsof -i:80)  
+sduo python3 ~/app_server/run.py  
--- data/distance.py
+++ data/distance.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from geopy.distance import geodesic
+import datetime
 
 
 def calculate_distance() -> dict:
@@ -7,7 +8,7 @@ def calculate_distance() -> dict:
 
     # {"AMBER 45": [(' POSTAL', ' BLK_NO', 'LATITUDE', ' LONGITUDE'), (' POSTAL', ' BLK_NO', 'LATITUDE', ' LONGITUDE')],
     #  "19 NASSIM": [(' POSTAL', ' BLK_NO', 'LATITUDE', ' LONGITUDE')]}
-    projects = pd.read_csv("AllProjects.csv", sep='|').groupby('project')[' POSTAL', ' BLK_NO', 'LATITUDE', ' LONGITUDE']\
+    projects = pd.read_csv("AllProjects.csv", sep='|').fillna('').groupby('project')[' POSTAL', ' BLK_NO', 'LATITUDE', ' LONGITUDE', 'street', 'developerName', ' ADDRESS'] \
         .apply(lambda g: list(map(tuple, g.values.tolist())))\
         .to_dict()
 
@@ -33,6 +34,9 @@ def calculate_distance() -> dict:
                     blk_within_1km['projectBlkNum'] = blk_within_1km.get('projectBlkNum', '') + f'{blk[1]},'
                     blk_within_1km['projectBlkLatitude'] = blk[2]
                     blk_within_1km['projectBlkLongitude'] = blk[3]
+                    blk_within_1km['street'] = blk[4]
+                    blk_within_1km['developerName'] = blk[5]
+                    blk_within_1km['address'] = blk[6]
                     blk_within_1km['distance'] = distance
                 if 1 < distance <= 2:
                     blk_within_2km['schoolPostal'] = postal
@@ -42,6 +46,9 @@ def calculate_distance() -> dict:
                     blk_within_2km['projectBlkNum'] = blk_within_1km.get('projectBlkNum', '') + f'{blk[1]},'
                     blk_within_2km['projectBlkLatitude'] = blk[2]
                     blk_within_2km['projectBlkLongitude'] = blk[3]
+                    blk_within_2km['street'] = blk[4]
+                    blk_within_2km['developerName'] = blk[5]
+                    blk_within_2km['address'] = blk[6]
                     blk_within_2km['distance'] = distance
             if blk_within_1km:
                 output.append(blk_within_1km)
@@ -56,6 +63,9 @@ def calculate_distance() -> dict:
     projectBlkPostal = {}
     projectBlkLatitude = {}
     projectBlkLongitude = {}
+    street = {}
+    developerName = {}
+    address = {}
     distance = {}  # 3 types of distance, <=1, 1-2, >2
     for i in range(0, len(output)):
         schoolPostal[i] = output[i]['schoolPostal']
@@ -65,6 +75,9 @@ def calculate_distance() -> dict:
         projectBlkNum[i] = output[i]['projectBlkNum']
         projectBlkLatitude[i] = output[i]['projectBlkLatitude']
         projectBlkLongitude[i] = output[i]['projectBlkLongitude']
+        street[i] = output[i]['street']
+        developerName[i] = output[i]['developerName']
+        address[i] = output[i]['address']
         distance[i] = output[i]['distance']
 
     ret = {}
@@ -75,20 +88,44 @@ def calculate_distance() -> dict:
     ret['projectBlkNum'] = projectBlkNum
     ret['projectBlkLatitude'] = projectBlkLatitude
     ret['projectBlkLongitude'] = projectBlkLongitude
+    ret['street'] = street
+    ret['developerName'] = developerName
+    ret['address'] = address
     ret['distance'] = distance
     return ret
 
 
-def write_to_csv(d: dict):
+def clean_transactions() -> dict:
+    TYPE_OF_SALE_CODES = {1: "New Sale", 2: "Sub Sale", 3: "Resale"}
+    SM_TO_SFT = 10.7639
+    transactions = pd.read_csv("PrivateProjectTransactions_.csv", sep='|').to_dict()
+    type_of_sale = transactions["typeOfSale"]
+    contract_date = transactions["contractDate"]
+    area = transactions["area"]
+    price = transactions["price"]
+    unit_price = {}
+
+    for row, dt in contract_date.items():
+        area[row] = round(area[row]*SM_TO_SFT, 2)
+        unit_price[row] = round(price[row]/area[row], 2)
+        contract_date[row] = datetime.datetime.strptime(str(dt).zfill(4), '%m%y').strftime('%Y-%m-%dT%H:%M:%S.%f')
+        type_of_sale[row] = TYPE_OF_SALE_CODES[type_of_sale[row]]
+
+    transactions["contractDate"] = contract_date
+    transactions["unitPrice"] = unit_price
+    return transactions
+
+
+def write_to_csv(d: dict, file_name: str):
     df = pd.DataFrame.from_dict(d)
-    df.to_csv('SchoolProjectDistance_.csv', sep='|', index=False)
+    df.to_csv(file_name, sep='|', index=False)
 
 
 def clean_csv():
-    schools = pd.read_csv("SchoolWithRanking.csv", sep='|')
-    schools.to_csv("SchoolWithRanking_comma.csv", index=False)
+    schools = pd.read_csv("SchoolProjectDistance.csv", sep='|')
+    schools.to_csv("SchoolProjectDistance_comma.csv", index=False)
 
 
 if __name__ == "__main__":
-    # write_to_csv(calculate_distance())
+    # write_to_csv(calculate_distance(), 'SchoolProjectDistance.csv')
     clean_csv()
